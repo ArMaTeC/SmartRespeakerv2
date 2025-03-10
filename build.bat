11da8782087a6de6fb12da32dca421f7cdc01452
Karl Lawrence <armatec0@gmail.com>
2025-03-10 17:20:34 +0000

    ff

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
-rem esphome -q update-all . >> Buildlog.log 2>&1
-rem esphome compile SmartReSpeaker.yaml 2>&1 | tee -a Buildlog.log
-esphome run SmartReSpeaker.yaml 2>&1 | tee -a RunBuildlog.log
+esphome run smartv2-factory.yaml 2>&1 | tee -a RunBuildlog.log
 pause
